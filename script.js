4e025728d224a186aa805e68db42d97a933d69da
Jamil-4khtar <jamil4khtar@gmail.com>
2024-10-05 11:10:58 +0530

    created a submit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const h1 = document.querySelector('#sizeInfo h1');
+
+let height = window.innerHeight;
+let width = window.innerWidth;
+
+h1.innerHTML = `Width: ${width} and Height: ${height}`;
+
+window.addEventListener("resize", () => {
+    let height = window.innerHeight;
+    let width = window.innerWidth;
+    h1.innerHTML = `Width: ${width} and Height: ${height}`;
+    
+})
